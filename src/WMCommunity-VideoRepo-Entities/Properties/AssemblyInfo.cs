89eff9d0982b344ad9dd8e3e329355f4d8a15069
Pau Castells <pau.castells@tokiota.com>
2017-01-10 11:51:04 +0100

    Solution Structure

---
+++ src/WMCommunity-VideoRepo-Entities/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WMCommunity_VideoRepo_Entities")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("08ee8c54-e4b3-4bcd-8183-f6497b4c008f")]
